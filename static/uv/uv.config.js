d459502ddec711047301c2c0b0da23fc24dd76e1
dasjdw0d <joeylentz0525@gmail.com>
2024-11-25 00:10:36 +0000

    update

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv/uv.handler.js', // handler
-    bundle: '/uv/uv.bundle.js', // bundle
-    config: '/uv/uv.config.js', // config
-    sw: '/uv/uv.sw.js', // sw
+    handler: '/uv/uv.handler.js',
+    client: '/uv/uv.client.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
 };
